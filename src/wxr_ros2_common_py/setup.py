3b5401edd5babf04e47fa88baa6b6768d5762df8
Jaehong-VM <jaehong.yoo@wrl.onl>
2025-09-09 11:29:17 +0900

    Init: wxr_ros2_common workspace

---
+++ src/wxr_ros2_common_py/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'wxr_ros2_common_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jaeyoo',
+    maintainer_email='galy960610@gmail.com',
+    description='Common Python utils for WXR ROS2',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
